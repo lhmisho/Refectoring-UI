c753d2abd0a57704b14cbcca764e1f1031adb44c
lhmisho <lhmisho@gmail.com>
2019-12-05 15:41:16 +0600

    some styles from aminul bhaii

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Login from './components/auth/login'
 import SearchFlight from './components/flight/flight-search'
+import BG from './static/images/bg.jpg'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="bg-gray-300 min-h-screen">
+      <div className="bg-scroll min-h-screen" style={{backgroundImage: `url(${BG})`}}>
         <div className="container mx-auto px-4" >
           <SearchFlight />
         </div>
--- src/components/flight/flight-search.jsx
+++ src/components/flight/flight-search.jsx
@@ -1,47 +1,59 @@
 import React from 'react'
-
+import Logo from '../../static/images/logo.png'
 
 const SearchFlight = () => {
     return (
         <div className="">
-            <div className="w-full max-w-lg p-4 mx-auto text-3xl">
-                Find your best deals on flight now
+            <div className="flex mb-4">
+                <div className="w-1/2">
+                    <div className="w-full max-w-lg p-4 mx-auto text-3xl">
+                        <img className="w-20 h-20" src={Logo} />
+                    </div>
+                    <div className="w-full max-w-lg p-4 mx-auto">
+                        <h1 className="text-3xl">Find your best deals on flight now</h1>
+                    </div>
+                    <div className="w-full max-w-lg p-4 mx-auto">
+                        <p>Efficiently target client-centric intellectual capital vis-a-vis leveraged experiences</p>
+                    </div>
+                </div>
+                <div class="w-1/2">
+                    <form className="w-full max-w-lg bg-white p-4 mx-auto rounded">
+                        <div className="flex flex-wrap -mx-3 mb-2">
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                    Leaving from ...
+                        </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" />
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap -mx-3 mb-2">
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                    Going to ...
+                        </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" />
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap -mx-3 mb-2">
+                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                    Depart
+                                </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="date" placeholder="Jane" />
+                            </div>
+                            <div className="w-full md:w-1/2 px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                    Return
+                                </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="Doe" />
+                            </div>
+                        </div>
+                        <div className="">
+                            <button className="bg-gray-900 m-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="button">Sign In</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <form className="w-full max-w-lg bg-white p-4 mx-auto rounded">
-                <div className="flex flex-wrap -mx-3 mb-2">
-                    <div className="w-full px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                            Leaving from ...
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" />
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-2">
-                    <div className="w-full px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                            Going to ...
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" />
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-2">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                            Depart
-                    </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="date" placeholder="Jane" />
-                    </div>
-                    <div className="w-full md:w-1/2 px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                            Return
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="date" placeholder="Doe" />
-                    </div>
-                </div>
-                <div className="">
-                    <button className="bg-gray-900 m-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
-                </div>
-            </form>
         </div>
     )
 }
