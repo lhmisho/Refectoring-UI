3326b5f5e29b7097e4837d06a440809a68d17153
lhmisho <lhmisho@gmail.com>
2020-01-21 00:46:06 +0600

    custom hooks

--- src/components/hooks/posts.jsx
+++ src/components/hooks/posts.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
 
-const Posts = () => {
-    const [count, setCount] = useState(0)
-    const [posts, setPosts] = useState([])
+/**
+ * creating custom hooks 
+ */
+function useData(url) {
+    const [data, setData] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [errors, setError] = useState('')
 
     useEffect(() => {
         setIsLoading(true)
-        Axios.get(BASE_URL)
+        Axios.get(url)
             .then(({ data }) => {
-                setPosts(data.slice(0, 10))
+                setData(data.slice(0, 5))
                 setIsLoading(false)
                 setError('')
             })
@@ -22,10 +24,24 @@ const Posts = () => {
                 setIsLoading(false)
                 console.log(error)
             })
-    }, [])
+    }, []);
+
+    return{
+        isLoading,
+        errors,
+        data
+    }
+}
+
+const Posts = () => {
+    const [count, setCount] = useState(0)
+    const {isLoading, errors, data: posts} = useData(`${BASE_URL}/posts`)
+    const {isLoading: isLoading2, errors: errors2, data: users} = useData(`${BASE_URL}/users`)
+
     useEffect(() => {
         console.log('count updated')
-    }, [count])
+    }, [])
+
     return (
         <div class="font-sans bg-grey-lighter flex flex-col min-h-screen w-full">
             <h2>Count: {count}</h2>
@@ -48,6 +64,34 @@ const Posts = () => {
                                 return (
                                     <tr key={idx}>
                                         <td class="border px-4 py-2">{item.title}</td>
+                                        <td class="border px-4 py-2">
+                                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <hr />
+            <h2>All users</h2>
+            {isLoading2 && <h2>Loading....</h2>}
+            {errors2 && <h2>Maybe some error occureds ....</h2>}
+            <div class="mb-6">
+                <div class="container mx-auto px-4 lg:rounded-lg">
+                    <table class="table-auto bg-white w-full shadow-2xl lg:rounded-lg">
+                        <thead class="bg-gray-200 text-gray-700 text-sm">
+                            <tr>
+                                <th class="border px-4 py-2">Name</th>
+                                <th class="border px-4 py-2">ACTIONS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((item, idx) => {
+                                return (
+                                    <tr key={idx}>
+                                        <td class="border px-4 py-2">{item.name}</td>
                                         <td class="border px-4 py-2">
                                             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
                                         </td>
